ccaa06b1d4622301ec0e6ea004eeb30667326e85
Saiful Islam <76849761+saifulIslamSourceCode@users.noreply.github.com>
2025-06-19 20:02:02 +0600

    setup react app
    created the todo component

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Todo from './Component/todoComponent'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Todo/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Component/todoComponent.tsx
@@ -0,0 +1,14 @@
+import { type JSX, useState } from "react";
+
+const Todo = (): JSX.Element => {
+
+    
+
+    return(
+        <div>
+            <h1>Todo</h1>
+        </div>
+    )
+}
+
+export default Todo;
